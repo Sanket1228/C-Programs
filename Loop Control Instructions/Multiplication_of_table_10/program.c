bd4d730fc3199bdb2536a4c86e2b256137cdaa02
Sanket1228 <sanketpatil1228@gmail.com>
2020-09-13 12:30:20 +0530

    C Programs

---
+++ Loop Control Instructions/Multiplication_of_table_10/program.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("********Multiplication of table 10********\n");
+    for(int i=1; i<=10; i++){
+        printf("10 X %d = %d\n",i,10*i);
+    }
+    return 0;
+}
---
+++ Loop Control Instructions/sum_of_first_10_integers/program.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int i = 1, sum = 0, n = 10;
+    
+    while(i<=n){
+        sum += i;
+        i++;
+    }
+    printf("The sum of 1 to 10 is %d\n",sum);
+    return 0;
+}
